3c429e8103aac2dba3cd2db41784b87140282d0a
Joona Romppanen <joona.romppanen@gmail.com>
2019-01-08 08:14:00 +0200

    Desktop environment

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -19,4 +19,6 @@ sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
 systemctl enable pacman-init.service choose-mirror.service
-systemctl set-default multi-user.target
+#systemctl set-default multi-user.target
+systemctl enable lxdm
+systemctl set-default graphical.target
